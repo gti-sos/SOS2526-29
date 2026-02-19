26d47b37e18a8637acbe092dbab88d6227d7621d
Alberto <alberto.l.m.j24@gmail.com>
2026-02-19 19:05:47 +0100

    Subir al proyecto de GitHub index-ALG

---
+++ index-ALG.js
@@ -0,0 +1,44 @@
+
+const datosDesastres = [
+    { country: 'afghanistan', year: 1950, death_count: 215, injured_count: 200, economic_damage_usd: 0 },
+    { country: 'afghanistan', year: 1960, death_count: 11, injured_count: 0, economic_damage_usd: 20 },
+    { country: 'afghanistan', year: 1970, death_count: 48, injured_count: 16, economic_damage_usd: 5200 },
+    { country: 'afghanistan', year: 1980, death_count: 58, injured_count: 352, economic_damage_usd: 26900 },
+    { country: 'afghanistan', year: 1990, death_count: 1039, injured_count: 395, economic_damage_usd: 8401 },
+    { country: 'afghanistan', year: 2000, death_count: 449, injured_count: 197, economic_damage_usd: 2511 },
+    { country: 'afghanistan', year: 2010, death_count: 263, injured_count: 5918, economic_damage_usd: 14800 },
+    { country: 'africa', year: 1900, death_count: 1112, injured_count: 0, economic_damage_usd: 0 },
+    { country: 'africa', year: 1910, death_count: 8501, injured_count: 0, economic_damage_usd: 0 },
+    { country: 'africa', year: 1920, death_count: 2701, injured_count: 0, economic_damage_usd: 0 }
+];
+
+const paisMuertes = 'afghanistan';
+
+const datosFiltrados = datosDesastres.filter(dato => dato.country === paisMuertes);
+
+
+if (datosFiltrados.length > 0) {
+
+    const totalMuertes = datosFiltrados
+                            .map(dato => dato.death_count)
+                            .reduce((suma, muertes) => suma + muertes, 0);
+
+   
+    const mediaMuertes = totalMuertes / datosFiltrados.length;
+
+    
+    console.log(`\n--- Estadísticas de Desastres Naturales ---`);
+    console.log(`País analizado: ${paisMuertes}`);
+    console.log(`Total de registros encontrados: ${datosFiltrados.length}`);
+    console.log(`La media de muertes es de: ${mediaMuertes.toFixed(2)} por registro/década.\n`);
+} else {
+    console.log(`No se encontraron datos para el país: ${paisMuertes}.`);
+}
+
+
+
+
+
+
+
+
