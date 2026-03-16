ca9be7ebb4bdc9a6f2335c36af8b6651f4d9dc63
Alberto <alberto.l.m.j24@gmail.com>
2026-03-16 16:01:51 +0100

    correcion /docs

--- src/back/v1/natural-disasters.js
+++ src/back/v1/natural-disasters.js
@@ -7,7 +7,7 @@ module.exports = function (app, db) {
 
     // 1. Redirección a la documentación (Requisito F06)
     app.get(BASE_API_URL + "/docs", (req, res) => {
-        res.redirect("https://documenter.getpostman.com/view/52437562/2sBXigNZSu"); 
+        res.redirect("https://documenter.getpostman.com/view/52437562/2sBXigNZbk"); 
     });
 
     // 2. Load Initial Data (GET) - Carga 10+ datos si está vacío
