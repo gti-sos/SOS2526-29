7b920ae77c192b7c4c384cee5e0205acbef88a0b
Alberto <alberto.l.m.j24@gmail.com>
2026-03-23 13:46:22 +0100

    correccion del home.svelte

--- frontend-group/src/routes/Home.svelte
+++ frontend-group/src/routes/Home.svelte
@@ -35,8 +35,8 @@
       name: "Alberto Lirola Gomez",
       resource: "natural-disasters",
       frontendUrl: "#/natural-disasters",
-      apiUrl: `${API_ORIGIN}/api/v1/natural-disasters`,
-      docsUrl: `${API_ORIGIN}/api/v1/natural-disasters/docs`
+      apiUrl: `${API_ORIGIN}/api/v2/natural-disasters`,
+      docsUrl: `${API_ORIGIN}/api/v2/natural-disasters/docs`
     }
   ];
 </script>
